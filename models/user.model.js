8c690b50304acd6d699052fc9bacaeec3bb9b4a7
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-04-09 11:13:39 +0530

    Setup for CRM

---
+++ models/user.model.js
@@ -0,0 +1,11 @@
+/**
+ * This file will Hold the Schema for the User Resourcee !
+ */
+
+const mongoose = require("mongoose");
+
+const 
+
+/**
+ * name , userId, password , email , createdAt , 
+ */
