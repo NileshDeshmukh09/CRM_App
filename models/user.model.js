6b254976878d548414fcb97736092c2585f90b20
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-04-09 11:44:13 +0530

    Added User DataBase

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,68 @@
 /**
  * This file will Hold the Schema for the User Resourcee !
- */
+*/
 
 const mongoose = require("mongoose");
 
-const 
+const userSchema = new  mongoose.Schema({
 
-/**
- * name , userId, password , email , createdAt , 
- */
+    /**
+     * Name , UserID , password , email , createdAt , 
+     * userType [ ADMIN | ENGINEER | CUSTOMER ],
+     * userStatus { Pending | Approved | Rejected}
+     */
+    name : {
+        type : String,
+        required : true
+    },
+
+    userID : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+
+    password : {
+        type : String,
+        required : true,
+    },
+
+    email : {
+        type : String,
+        required : true,
+        lowercase : true,
+        minlength : true,
+        unique : true,
+    },
+
+    createdAt : {
+        type : Date , 
+        immutable : true,
+        default : ()=> {
+            return Date.now();
+        }
+    },
+
+    updatedAt : {
+        type : Date ,
+        default : () => {
+            return Date.now();
+        }
+    },
+
+    userType : {
+         type : String ,
+         required : true ,
+         default : "CUSTOMER",
+    },
+
+    userStatus : {
+        type :String,
+        required : true,
+        default :"APPROVED"
+    }
+
+});
+
+
+module.exports = mongoose.model("User" , userSchema);
