69aa3de8875cfd0f1f17d54988a0ddd4a83f84e0
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-07-13 22:28:45 +0530

    Authorized ticket Routes  to access only  verified Users

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -1,6 +1,10 @@
 
 const ticketController = require("../controllers/ticket.controller");
+const { authJWT } = require("../middlewares");
 
 module.exports = ( app ) => {
-    app.post("/crm/api/v1/tickets" , ticketController.createTicket );
+    /**
+     * POST - 127.0.0.8080/crm/api/v1/tickets
+     */
+    app.post("/crm/api/v1/tickets" ,[ authJWT.verifyToken ], ticketController.createTicket );
 }
